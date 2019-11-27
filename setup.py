7fbdfe37660bca92bfc866178b867f181aaab34c
Tom de Geus <tom@geus.me>
2019-11-27 07:50:53 +0100

    Cleanup

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(
-  name = 'GooseHDF5',
-  version = '0.1.0',
-  license = 'MIT',
-  author = 'Tom de Geus',
-  author_email = 'tom@geus.me',
-  description = 'Wrapper around h5py',
-  long_description = 'Wrapper around h5py',
-  keywords = 'HDF5, h5py',
-  url = 'https://github.com/tdegeus/GooseHDF5',
-  packages = find_packages(),
-  install_requires = ['docopt>=0.6.2', 'h5py>=2.8.0'],
-  entry_points = {
-        'console_scripts': [
-          'G5check = GooseHDF5.cli.G5check:main',
-          'G5compare = GooseHDF5.cli.G5compare:main',
-          'G5list = GooseHDF5.cli.G5list:main',
-          'G5repair = GooseHDF5.cli.G5repair:main']})
+     name = 'GooseHDF5',
+     version = '0.1.0',
+     license = 'MIT',
+     author = 'Tom de Geus',
+     author_email = 'tom@geus.me',
+     description = 'Wrapper around h5py',
+     long_description = 'Wrapper around h5py',
+     keywords = 'HDF5, h5py',
+     url = 'https://github.com/tdegeus/GooseHDF5',
+     packages = find_packages(),
+     install_requires = ['docopt>=0.6.2', 'h5py>=2.8.0'],
+     entry_points = {
+          'console_scripts': [
+               'G5check = GooseHDF5.cli.G5check:main',
+               'G5compare = GooseHDF5.cli.G5compare:main',
+               'G5list = GooseHDF5.cli.G5list:main',
+               'G5repair = GooseHDF5.cli.G5repair:main']})
