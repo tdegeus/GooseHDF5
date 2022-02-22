ba2616c8cbcb772621bb5f7de587c592d9c241ae
Tom de Geus <tom@geus.me>
2022-02-22 14:14:05 +0100

    Fixing typo in G5print

--- GooseHDF5/cli/G5print.py
+++ GooseHDF5/cli/G5print.py
@@ -98,14 +98,14 @@ def main():
                 datasets = list(
                     getdatasets(source, root=args.root, max_depth=args.max_depth, fold=args.fold)
                 )
-                datasets += list(getgroups(source, has_attrs=True))
+                datasets += list(getgroups(source, root=args.root, has_attrs=True))
                 datasets = sorted(datasets)
             elif args.regex:
                 print_header = True
                 paths = list(
                     getdatasets(source, root=args.root, max_depth=args.max_depth, fold=args.fold)
                 )
-                paths += list(getgroups(source, has_attrs=True))
+                paths += list(getgroups(source, root=args.root, has_attrs=True))
                 datasets = []
                 for dataset in args.dataset:
                     datasets += [path for path in paths if re.match(dataset, path)]
