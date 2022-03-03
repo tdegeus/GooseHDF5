76bf35b5c121e2b47e4d4e3948400068ce27c07c
Tom de Geus <tom@geus.me>
2022-03-03 13:16:47 +0100

    Minor style change `setup.py`

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from setuptools import find_packages
 from setuptools import setup
 
+project_name = "GooseHDF5"
+
 setup(
-    name="GooseHDF5",
+    name=project_name,
     license="MIT",
     author="Tom de Geus",
     author_email="tom@geus.me",
     description="Wrapper around h5py",
     long_description="Wrapper around h5py",
     keywords="HDF5, h5py",
-    url="https://github.com/tdegeus/GooseHDF5",
+    url=f"https://github.com/tdegeus/{project_name:s}",
     packages=find_packages(),
-    use_scm_version={"write_to": "GooseHDF5/_version.py"},
+    use_scm_version={"write_to": f"{project_name}/_version.py"},
     setup_requires=["setuptools_scm"],
     install_requires=["h5py", "click"],
     entry_points={
