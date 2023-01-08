79e4cea93daba26b04bb58f44dd9c39d4bd51e4f
Tom de Geus <tom@geus.me>
2023-01-08 10:04:10 +0100

    Add `ExtendableList`

--- tests/test_dump.py
+++ tests/test_dump.py
@@ -1,20 +1,26 @@
-import os
+import pathlib
 import shutil
 import unittest
 
 import h5py
+import numpy as np
 
 import GooseHDF5 as g5
 
+root = pathlib.Path(__file__).parent
+testdir = root / "mytest"
 
-class Test_itereator(unittest.TestCase):
+
+class Test_iterator(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        testdir.mkdir(exist_ok=True)
+
+    @classmethod
+    def tearDownClass(self):
+        shutil.rmtree(testdir)
+
     def test_dump(self):
-
-        dirname = "mytest"
-        filepath = os.path.join(dirname, "bar.h5")
-
-        if not os.path.isdir(dirname):
-            os.makedirs(dirname)
 
         A = {
             "foo": {"a": 1, "b": 2},
@@ -27,7 +33,7 @@ class Test_itereator(unittest.TestCase):
             for b in A[a]:
                 paths.append(f"/{a}/{b}")
 
-        with h5py.File(filepath, "w") as file:
+        with h5py.File(testdir / "foo.h5", "w") as file:
 
             g5.dump(file, A)
 
@@ -37,7 +43,43 @@ class Test_itereator(unittest.TestCase):
                 for b in A[a]:
                     self.assertEqual(file[a][b][...], A[a][b])
 
-        shutil.rmtree(dirname)
+
+class Test_ExtendableList(unittest.TestCase):
+    """
+    Test the extendable list class
+    """
+
+    @classmethod
+    def setUpClass(self):
+        testdir.mkdir(exist_ok=True)
+
+    @classmethod
+    def tearDownClass(self):
+        shutil.rmtree(testdir)
+
+    def test_append(self):
+
+        data = np.random.random([100])
+
+        with h5py.File(testdir / "foo.h5", "w") as file:
+
+            with g5.ExtendableList(file, "foo", data.dtype, chunk=9) as dset:
+                for d in data:
+                    dset.append(d)
+
+            self.assertTrue(np.allclose(data, file["foo"][...]))
+
+    def test_add(self):
+
+        data = np.random.random([10, 10])
+
+        with h5py.File(testdir / "foo.h5", "w") as file:
+
+            with g5.ExtendableList(file, "foo", data.dtype, chunk=9) as dset:
+                for d in data:
+                    dset += d
+
+            self.assertTrue(np.allclose(data.ravel(), file["foo"][...]))
 
 
 if __name__ == "__main__":
