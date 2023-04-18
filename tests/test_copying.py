0ad7eb17b354105ef867162d805b27cb27e1fcb2
Tom de Geus <tom@geus.me>
2023-04-18 14:09:39 +0200

    Adding tests

--- tests/test_copying.py
+++ tests/test_copying.py
@@ -33,39 +33,20 @@ class Test_itereator(unittest.TestCase):
 
         datasets = ["/a", "/b/foo", "/c/d/foo"]
 
-        with h5py.File(basedir / "foo_1.h5", "w") as source:
-            with h5py.File(basedir / "bar_1.h5", "w") as dest:
+        with h5py.File(basedir / "a.h5", "w") as source, h5py.File(basedir / "b.h5", "w") as dest:
 
-                for d in datasets:
-                    source[d] = np.random.rand(10)
+            for d in datasets:
+                source[d] = np.random.rand(10)
 
-                g5.copy(source, dest, datasets)
+            g5.copy(source, dest, datasets)
 
-                for path in datasets:
-                    self.assertTrue(g5.equal(source, dest, path))
+            for path in datasets:
+                self.assertTrue(g5.equal(source, dest, path))
 
     def test_copy_softlinks(self):
-
-        datasets = ["/a", "/b/foo", "/c/d/foo"]
-        links = ["/mylink/a", "/mylink/b/foo", "/mylink/c/d/foo"]
-
-        with h5py.File(basedir / "foo_1.h5", "w") as source:
-            with h5py.File(basedir / "bar_1.h5", "w") as dest:
-
-                for link, d in zip(links, datasets):
-                    source[d] = np.random.rand(10)
-                    source[link] = h5py.SoftLink(d)
-
-                g5.copy(source, dest, datasets + links, preserve_soft=True)
-
-                for path in datasets + links:
-                    self.assertTrue(g5.equal(source, dest, path))
-                for path in datasets:
-                    self.assertNotIsInstance(dest.get(path, getlink=True), h5py.SoftLink)
-                for path in links:
-                    self.assertIsInstance(dest.get(path, getlink=True), h5py.SoftLink)
-
-    def test_copy_expand_softlinks(self):
+        """
+        Default: SoftLink -> HardLink
+        """
 
         datasets = ["/a", "/b/foo", "/c/d/foo"]
         links = ["/mylink/a", "/mylink/b/foo", "/mylink/c/d/foo"]
@@ -80,23 +61,47 @@ class Test_itereator(unittest.TestCase):
 
             for path in datasets + links:
                 self.assertTrue(g5.equal(source, dest, path))
-            for path in datasets + links:
+            for path in datasets:
                 self.assertNotIsInstance(dest.get(path, getlink=True), h5py.SoftLink)
+            for path in links:
+                self.assertIsInstance(dest.get(path, getlink=True), h5py.HardLink)
+
+    def test_copy_preserve_softlinks(self):
+        """
+        Custom: SoftLink -> SoftLink
+        """
+
+        datasets = ["/a", "/b/foo", "/c/d/foo"]
+        links = ["/mylink/a", "/mylink/b/foo", "/mylink/c/d/foo"]
+
+        with h5py.File(basedir / "a.h5", "w") as source, h5py.File(basedir / "b.h5", "w") as dest:
+
+            for link, d in zip(links, datasets):
+                source[d] = np.random.rand(10)
+                source[link] = h5py.SoftLink(d)
+
+            g5.copy(source, dest, datasets + links, preserve_soft=True)
+
+            for path in datasets + links:
+                self.assertTrue(g5.equal(source, dest, path))
+            for path in datasets:
+                self.assertNotIsInstance(dest.get(path, getlink=True), h5py.SoftLink)
+            for path in links:
+                self.assertIsInstance(dest.get(path, getlink=True), h5py.SoftLink)
 
     def test_copy_skip(self):
 
         datasets = ["/a", "/b/foo", "/c/d/foo"]
 
-        with h5py.File(basedir / "foo_1.h5", "w") as source:
-            with h5py.File(basedir / "bar_1.h5", "w") as dest:
+        with h5py.File(basedir / "a.h5", "w") as source, h5py.File(basedir / "b.h5", "w") as dest:
 
-                for d in datasets:
-                    source[d] = np.random.rand(10)
+            for d in datasets:
+                source[d] = np.random.rand(10)
 
-                g5.copy(source, dest, datasets + ["/nonexisting"], skip=True)
+            g5.copy(source, dest, datasets + ["/nonexisting"], skip=True)
 
-                for path in datasets:
-                    self.assertTrue(g5.equal(source, dest, path))
+            for path in datasets:
+                self.assertTrue(g5.equal(source, dest, path))
 
     def test_copy_shallow(self):
 
@@ -117,56 +122,56 @@ class Test_itereator(unittest.TestCase):
             self.assertFalse(g5.exists(dest, "/b/bar"))
 
     def test_copy_recursive(self):
+        """
+        Default: recursive copy
+        """
 
         datasets = ["/a", "/b/foo", "/b/bar", "/c/d/foo"]
 
-        with h5py.File(basedir / "foo_1.h5", "w") as source:
-            with h5py.File(basedir / "bar_1.h5", "w") as dest:
+        with h5py.File(basedir / "a.h5", "w") as source, h5py.File(basedir / "b.h5", "w") as dest:
 
-                for d in datasets:
-                    source[d] = np.random.rand(10)
+            for d in datasets:
+                source[d] = np.random.rand(10)
 
-                g5.copy(source, dest, ["/a", "/b", "/c/d/foo"])
+            g5.copy(source, dest, ["/a", "/b", "/c"])
 
-                for path in datasets:
-                    self.assertTrue(g5.equal(source, dest, path))
+            for path in datasets:
+                self.assertTrue(g5.equal(source, dest, path))
 
     def test_copy_attrs(self):
 
         datasets = ["/a", "/b/foo", "/c/d/foo"]
 
-        with h5py.File(basedir / "foo_1.h5", "w") as source:
-            with h5py.File(basedir / "bar_1.h5", "w") as dest:
+        with h5py.File(basedir / "a.h5", "w") as source, h5py.File(basedir / "b.h5", "w") as dest:
 
-                for d in datasets:
-                    source[d] = np.random.rand(10)
+            for d in datasets:
+                source[d] = np.random.rand(10)
 
-                meta = source.create_group("/meta")
-                meta.attrs["version"] = np.random.rand(10)
+            meta = source.create_group("/meta")
+            meta.attrs["version"] = np.random.rand(10)
 
-                datasets += ["/meta"]
-                g5.copy(source, dest, datasets)
+            datasets += ["/meta"]
+            g5.copy(source, dest, datasets)
 
-                for path in datasets:
-                    self.assertTrue(g5.equal(source, dest, path))
+            for path in datasets:
+                self.assertTrue(g5.equal(source, dest, path))
 
     def test_copy_groupattrs(self):
 
         datasets = ["/a", "/b/foo", "/c/d/foo"]
 
-        with h5py.File(basedir / "foo_1.h5", "w") as source:
-            with h5py.File(basedir / "bar_1.h5", "w") as dest:
+        with h5py.File(basedir / "a.h5", "w") as source, h5py.File(basedir / "b.h5", "w") as dest:
 
-                for d in datasets:
-                    source[d] = np.random.rand(10)
+            for d in datasets:
+                source[d] = np.random.rand(10)
 
-                source["/b"].attrs["version"] = np.random.rand(10)
+            source["/b"].attrs["version"] = np.random.rand(10)
 
-                datasets += ["/b"]
-                g5.copy(source, dest, datasets)
+            datasets += ["/b"]
+            g5.copy(source, dest, datasets)
 
-                for path in datasets:
-                    self.assertTrue(g5.equal(source, dest, path))
+            for path in datasets:
+                self.assertTrue(g5.equal(source, dest, path))
 
     def test_copy_root(self):
 
@@ -174,18 +179,17 @@ class Test_itereator(unittest.TestCase):
         source_pre = "/my/source"
         dest_pre = "/your/dest"
 
-        with h5py.File(basedir / "foo_1.h5", "w") as source:
-            with h5py.File(basedir / "bar_1.h5", "w") as dest:
+        with h5py.File(basedir / "a.h5", "w") as source, h5py.File(basedir / "b.h5", "w") as dest:
 
-                for d in datasets:
-                    source[g5.join(source_pre, d)] = np.random.rand(10)
+            for d in datasets:
+                source[g5.join(source_pre, d)] = np.random.rand(10)
 
-                g5.copy(source, dest, datasets, source_root=source_pre, root=dest_pre)
+            g5.copy(source, dest, datasets, source_root=source_pre, root=dest_pre)
 
-                for path in datasets:
-                    s = g5.join(source_pre, path)
-                    d = g5.join(dest_pre, path)
-                    self.assertTrue(g5.equal(source, dest, s, d))
+            for path in datasets:
+                s = g5.join(source_pre, path)
+                d = g5.join(dest_pre, path)
+                self.assertTrue(g5.equal(source, dest, s, d))
 
 
 if __name__ == "__main__":
